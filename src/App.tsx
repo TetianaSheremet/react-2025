0a57eb403340216e8cc1edf2bf45be40759a8a14
TanyaCodeQueen <tanya.she01@gmail.com>
2025-11-07 13:54:30 -0500

    useFetch hook created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import './App.css'
 
+import {useFetch} from "./hooks/useFetch.tsx";
+
+
+
 function App() {
 
-
+    const users = useFetch<{id: number,name: string}[]>("https://jsonplaceholder.typicode.com/users",[])
   return (
-    <>
-      hello world
-    </>
+    <div>
+        {
+            users.map((user) => (
+                <div key={user.id}>
+                    {user.id} {user.name}
+                </div>
+            ))
+        }
+    </div>
   )
 }
 
